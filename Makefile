eca036d9449511d4ac1a11ec6efde7880675ec59
rickNoise <emberix@gmail.com>
2025-08-04 12:37:43 +1000

    basic project setup

---
+++ Makefile
@@ -0,0 +1,2 @@
+run:
+	go build -o out && ./out
--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+/* CONSTANTS */
+const SERVER_ADDR = ":8080"
 
 func main() {
-	fmt.Println("Hello world!")
+	serveMux := http.NewServeMux()
+
+	s := http.Server{
+		Addr:    SERVER_ADDR,
+		Handler: serveMux,
+	}
+	fmt.Printf("created new http.Server\n")
+
+	fmt.Printf("listening on Addr: %v", s.Addr)
+	err := s.ListenAndServe()
+	if err != nil {
+		log.Fatalf("error on ListenAndServe: %v", err)
+	}
 }

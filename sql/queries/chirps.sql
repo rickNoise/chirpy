ce97401fddd401bea9749a50015522af03122b99
rickNoise <emberix@gmail.com>
2025-08-08 15:59:54 +1000

    Add a new query that retrieves all chirps in ascending order by created_at

--- sql/queries/chirps.sql
+++ sql/queries/chirps.sql
@@ -8,3 +8,11 @@ INSERT INTO
         user_id
     )
 VALUES (NOW(), NOW(), @body, @user_id) RETURNING *;
+
+-- name: GetAllChirps :many
+-- Retrieves all chirps in ascending order by created_at.
+SELECT (
+        id, created_at, updated_at, body, user_id
+    )
+FROM chirps
+ORDER BY created_at ASC;

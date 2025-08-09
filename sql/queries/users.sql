7a254dcdf655c160164c9c58e637a5ee48771ec9
rickNoise <emberix@gmail.com>
2025-08-09 12:54:51 +1000

    updated CreateUser query to account for new hashed_password field

--- sql/queries/users.sql
+++ sql/queries/users.sql
@@ -1,8 +1,18 @@
 -- name: CreateUser :one
 -- id PK for users has a default UUID generated, so can leave out here
 INSERT INTO
-    users (created_at, updated_at, email)
-VALUES (NOW(), NOW(), @email) RETURNING *;
+    users (
+        created_at,
+        updated_at,
+        email,
+        hashed_password
+    )
+VALUES (
+        NOW(),
+        NOW(),
+        @email,
+        @hashedPassword
+    ) RETURNING *;
 
 -- name: DeleteAllUsers :exec
 -- deletes all users data in the users table
